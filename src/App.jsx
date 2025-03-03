463bc0052376156aab09b9bd0036b158ff7a8a13
Julio Guerrero Alvarez <judaguer@espol.edu.ec>
2025-03-03 18:58:42 -0500

    add: image-slider terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
-import Accordian from './components/acordion'
-import RandomColor from './components/random-color'
-import StarRating from './components/star-rating'
+import "./App.css";
+import Accordian from "./components/acordion";
+import ImageSlider from "./components/image-slider";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
-
   return (
-    <div className='App'>
+    <div className="App">
       {/* Acordion */}
       {/* <Accordian/> */}
 
@@ -14,9 +14,16 @@ function App() {
       {/* <RandomColor/> */}
 
       {/* Estrellas */}
-      <StarRating numOfStars={10}/>
+      {/* <StarRating numOfStars={10}/> */}
+
+      {/* Image Slider */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        limit={"10"}
+        page={"1"}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
