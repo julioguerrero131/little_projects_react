de38208eabec6ef595ee6a774fbb3ef6b587fa2c
Julio Guerrero Alvarez <judaguer@espol.edu.ec>
2025-03-02 23:13:02 -0500

    random-color terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Accordian from './components/acordion'
+import RandomColor from './components/random-color'
 
 function App() {
 
   return (
     <div className='App'>
       {/* Acordion */}
-      <Accordian/>
+      {/* <Accordian/> */}
+
+      {/* Color Random */}
+      <RandomColor/>
     </div>
   )
 }
